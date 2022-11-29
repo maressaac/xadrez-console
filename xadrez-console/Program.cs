46bb991f5498cd1c515875933ad76c42bae249a8
Maressa Cortelete <maressacortelete@gmail.com>
2022-11-29 19:10:38 -0300

    Adicionando console de tela

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,12 +12,9 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
-            Console.WriteLine("Posicao " + P);
-            Console.ReadLine();
+           Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
         }
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -13,11 +13,15 @@ namespace tabuleiro
 
         private Peca[,] pecas;   
 
-        public Tabuleiro(int linha, int colunas)
+        public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
             this.colunas = colunas;
-            this.pecas = new Peca[linhas, colunas];
+            pecas = new Peca[linhas, colunas];
+        }
+        public Peca peca(int linha, int coluna)
+        {
+            return pecas[linha, coluna];
         }
     }
 }

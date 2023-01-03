28efa12b2819aff628f5ae9aaedb4c17fd8e26ab
Maressa Albuquerque Cortelete <maressacortelete@gmail.com>
2023-01-03 17:29:26 -0300

    jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -136,8 +136,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não se pode colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+            // #jogada especial PROMOCAO
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
+
                 xeque = true;
             }
             else
@@ -153,7 +167,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            
 
             // #Jogada especial EN PASSANT
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
